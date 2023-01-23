948c3a71e82374c0181088893ea4ede79de99383
Tushar <singhtushar348@gmail.com>
2023-01-24 02:20:56 +0530

    Create users schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_082443) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_173516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_082443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "alias"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "links", "users"
 end
