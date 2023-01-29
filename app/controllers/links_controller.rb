e3ae6a02260064ebc2e04c6d864216f4c7b5319a
Tushar <singhtushar348@gmail.com>
2023-01-30 00:08:34 +0530

    Check Current user present with before_action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,13 @@
 class LinksController < ApplicationController
+  before_action do
+    unless Current.user
+      flash[:alert] = "Please login for access"
+      redirect_to root_path
+    end
+  end
+
   def index
-    @links = Link.all
+    @links = Current.user.links.all
   end
 
   def new
@@ -9,11 +16,12 @@ class LinksController < ApplicationController
 
   def create
     link = Link.new(link_params)
+    link.user = Current.user
     if link.alias.empty?
       link.short_url = link.generate_mini_url
     elsif Link.exists?(alias: "#{link.alias}")
       flash[:alert] = "alias is taken please try another"
-      redirect_to root_path
+      redirect_to new_link_path
       return
     else
       link.short_url = "http://localhost:3000/#{link.alias}"
@@ -22,8 +30,7 @@ class LinksController < ApplicationController
       flash[:notice] = "You got it!"
       redirect_to links_path
     else
-      flash.now[:alert] = link.errors.full_messages.first
-      redirect_to root_path
+      render :new, status: 422
       return
     end
   end
