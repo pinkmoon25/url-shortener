c76c459aa329d8ad95728eebca764b22851991ac
Tushar <singhtushar348@gmail.com>
2023-01-30 00:10:37 +0530

    Add routes for login, signin and logout path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'links#new'
+  root 'registrations#new'
   resources :links, only: [:index, :new, :create, :show]
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+  delete 'logout', to: "sessions#destroy"
   get ':uuid', to: "links#visit_link"
 end
